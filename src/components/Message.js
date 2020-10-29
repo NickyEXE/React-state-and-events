d3e4df37cf5378bb573cb7485f600443b47d0530
NickyEXE <dovern42@gmail.com>
2020-10-29 18:00:29 -0400

    done

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,10 +1,29 @@
 import React from 'react'
+import LikeButton from './LikeButton'
 
 class Message extends React.Component {
 
+  // state = {
+  //   numberOfLikes: 6
+  // }
+
+  constructor(){
+    super()
+    this.state = {
+      numberOfLikes: 4,
+      potato: "batman"
+    }
+  }
+
+  onClick = (e) => {
+    this.setState({numberOfLikes: this.state.numberOfLikes + 1})
+  }
+
   render(){
-    console.log(this.props)
-  return (<li> { this.props.text } <button>{ this.props.numberOfLikes } likes</button> </li>)
+    console.log("in the render", this.state)
+    const {text} = this.props
+
+    return (<li> { text } <LikeButton numberOfLikes={this.state.numberOfLikes} onClick={this.onClick}/> </li>)
   }
 }
 
--- src/containers/MessageList.js
+++ src/containers/MessageList.js
@@ -10,7 +10,7 @@ class MessageList extends React.Component {
     <>
       <h3>Latest Messages:</h3>
       <ol>
-        {arr.map(text => <Message text={text} numberOfLikes={2} />)}
+        {arr.map((text, i) => <Message text={text} key={i} />)}
       </ol>
     </>
     )
